ba0d47ecef2c4ffb29de42dfcac7cdb38e506314
Nikita Zimin <nzeemin@gmail.com>
2022-07-15 23:06:24 +0300

    BAS loader, README.md

--- !compile.cmd
+++ !compile.cmd
@@ -5,12 +5,15 @@ if exist scubacodb.bin del scubacodb.bin
 if exist scubacodb.txt del scubacodb.txt
 if exist scubacodc.bin del scubacodc.bin
 if exist scubacodc.txt del scubacodc.txt
+if exist scuba.tap del scuba.tap
 
 rem Define ESCchar to use in ANSI escape sequences
 rem https://stackoverflow.com/questions/2048509/how-to-echo-with-different-colors-in-the-windows-command-line
 for /F "delims=#" %%E in ('"prompt #$E# & for %%E in (1) do rem"') do set "ESCchar=%%E"
 
 @echo on
+tools\bas2tap.exe -a1 -sSCUBA basloader.bas basloader.tap
+@if errorlevel 1 goto Failed
 tools\pasmo scubacoda.asm scubacoda.bin scubacoda.txt
 @if errorlevel 1 goto Failed
 tools\pasmo scubacodb.asm scubacodb.bin scubacodb.txt
@@ -20,6 +23,20 @@ tools\pasmo scubacodc.asm scubacodc.bin scubacodc.txt
 @echo off
 
 dir /-c scubacod?.bin|findstr /R /C:"scubacod"
+
+tools\bin2tap scubacoda.bin -o scubacoda.tap -a 16384
+@if errorlevel 1 goto Failed
+
+tools\bin2tap scubacodb.bin -o scubacodb.tap -a 24576
+@if errorlevel 1 goto Failed
+
+tools\bin2tap scubacodc.bin -o scubacodc.tap -a 55552
+@if errorlevel 1 goto Failed
+
+@echo on
+copy /B /Y basloader.tap+scubacoda.tap+scubacodb.tap+scubacodc.tap scuba.tap
+@if errorlevel 1 goto Failed
+@echo off
 
 echo %ESCchar%[92mSUCCESS%ESCchar%[0m
 exit
---
+++ README.md
@@ -0,0 +1,19 @@
+# spectrum-scubadive-rev
+Reversing **Scuba Dive** game for ZX Spectrum, 1983 Durell Software.
+
+Status: work in progress.
+
+
+
+## Tools for the `tools` folder
+
+ - `bas2tap.exe`, `bin2tap.exe` utilities
+   https://sourceforge.net/projects/zxspectrumutils/files/
+
+ - `pasmo.exe` cross-assembler
+   http://pasmo.speccy.org/
+
+## Links
+
+ - [Scuba Dive disasm in SkoolKit HTML](https://nzeemin.github.io/skoolkit-game-revs/scubadive-zx/scuba/)
+ - [nzeemin/skoolkit-game-revs repo for some game disasm using SkoolKit](https://github.com/nzeemin/skoolkit-game-revs)
